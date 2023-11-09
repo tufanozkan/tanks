c98772709956c7647bb41ec0dcbf68b782ebb96d
tufan özkan <tuf.ozkan@gmail.com>
2023-11-09 13:57:00 +0300

    files uploaded and fnished project

---
+++ Assets/Scripts/Tank/TankHealth.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TankHealth : MonoBehaviour
+{
+    public float m_StartingHealth = 100f;               
+    public Slider m_Slider;                             
+    public Image m_FillImage;                           
+    public Color m_FullHealthColor = Color.green;       
+    public Color m_ZeroHealthColor = Color.red;         
+    public GameObject m_ExplosionPrefab;                // A prefab that will be instantiated in Awake, then used whenever the tank dies.
+
+
+    private AudioSource m_ExplosionAudio;               
+    private ParticleSystem m_ExplosionParticles;        
+    private float m_CurrentHealth;                      
+    private bool m_Dead;                                // Has the tank been reduced beyond zero health yet?
+
+
+    private void Awake()
+    {
+        m_ExplosionParticles = Instantiate(m_ExplosionPrefab).GetComponent<ParticleSystem>();
+
+        m_ExplosionAudio = m_ExplosionParticles.GetComponent<AudioSource>();
+
+        // Disable the prefab so it can be activated when it's required.
+        m_ExplosionParticles.gameObject.SetActive(false);
+    }
+
+
+    private void OnEnable()
+    {
+        m_CurrentHealth = m_StartingHealth;
+        m_Dead = false;
+
+        // Update the health slider's value and color.
+        SetHealthUI();
+    }
+
+
+    public void TakeDamage(float amount)
+    {
+        m_CurrentHealth -= amount;
+
+        // Change the UI elements appropriately.
+        SetHealthUI();
+
+        if (m_CurrentHealth <= 0f && !m_Dead)
+        {
+            OnDeath();
+        }
+    }
+
+
+    private void SetHealthUI()
+    {
+        m_Slider.value = m_CurrentHealth;
+
+        // Interpolate the color of the bar between the choosen colours based on the current percentage of the starting health.
+        m_FillImage.color = Color.Lerp(m_ZeroHealthColor, m_FullHealthColor, m_CurrentHealth / m_StartingHealth);
+    }
+
+
+    private void OnDeath()
+    {
+        m_Dead = true;
+
+        // Move the instantiated explosion prefab to the tank's position and turn it on.
+        m_ExplosionParticles.transform.position = transform.position;
+        m_ExplosionParticles.gameObject.SetActive(true);
+
+        m_ExplosionParticles.Play();
+
+        m_ExplosionAudio.Play();
+
+        // Turn the tank off.
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Tank/TankShooting.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TankShooting : MonoBehaviour
+{
+    public int m_PlayerNumber = 1;              
+    public Rigidbody m_Shell;                   
+    public Transform m_FireTransform;           // A child of the tank where the shells are spawned.
+    public Slider m_AimSlider;                  // A child of the tank that displays the current launch force.
+    public AudioSource m_ShootingAudio;         
+    public AudioClip m_ChargingClip;            
+    public AudioClip m_FireClip;                
+    public float m_MinLaunchForce = 15f;        
+    public float m_MaxLaunchForce = 30f;        
+    public float m_MaxChargeTime = 0.75f;       // How long the shell can charge for before it is fired at max force.
+
+
+    private string m_FireButton;                
+    private float m_CurrentLaunchForce;         
+    private float m_ChargeSpeed;                
+    private bool m_Fired;                       // Whether or not the shell has been launched with this button press.
+
+
+    private void OnEnable()
+    {
+        m_CurrentLaunchForce = m_MinLaunchForce;
+        m_AimSlider.value = m_MinLaunchForce;
+    }
+
+
+    private void Start()
+    {
+        // The fire axis is based on the player number.
+        m_FireButton = "Fire" + m_PlayerNumber;
+
+        m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
+    }
+
+
+    private void Update()
+    {
+        // The slider should have a default value of the minimum launch force.
+        m_AimSlider.value = m_MinLaunchForce;
+
+        if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
+        {
+            m_CurrentLaunchForce = m_MaxLaunchForce;
+            Fire();
+        }
+        // Otherwise, if the fire button has just started being pressed...
+        else if (Input.GetButtonDown(m_FireButton))
+        {
+            m_Fired = false;
+            m_CurrentLaunchForce = m_MinLaunchForce;
+
+            m_ShootingAudio.clip = m_ChargingClip;
+            m_ShootingAudio.Play();
+        }
+        // Otherwise, if the fire button is being held and the shell hasn't been launched yet...
+        else if (Input.GetButton(m_FireButton) && !m_Fired)
+        {
+            m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
+
+            m_AimSlider.value = m_CurrentLaunchForce;
+        }
+        // Otherwise, if the fire button is released and the shell hasn't been launched yet...
+        else if (Input.GetButtonUp(m_FireButton) && !m_Fired)
+        {
+            Fire();
+        }
+    }
+
+
+    private void Fire()
+    {
+        m_Fired = true;
+
+        // Create an instance of the shell and store a reference to it's rigidbody.
+        Rigidbody shellInstance = Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+
+        shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward; ;
+
+        m_ShootingAudio.clip = m_FireClip;
+        m_ShootingAudio.Play();
+
+        // Reset the launch force.  This is a precaution in case of missing button events.
+        m_CurrentLaunchForce = m_MinLaunchForce;
+    }
+}
